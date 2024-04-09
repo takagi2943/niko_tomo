78edaf3391a474e7e255908e04cb315ecf073dae
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-04-09 05:57:40 +0000

    途中

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,4 +1,9 @@
 class Public::FavoritesController < ApplicationController
+  
+  # いいね（お気に入り）一覧
   def index
+    @user = User.find(params[:id])
+    @favorites = Favorite.where(user_id: @user.id).pluck(:music_post_id)
+    @favorite_posts = MusicPost.find(@favorites)
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :prohibit_multiple_login, if: :admin_signed_in?
+  before_action :is_active?, only: [:create]
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,10 +21,27 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  # ユーザー登録していたら下記の処理
+  def is_active?
+    @user = User.find_by(email: params[:user][:email])
+    return unless @user && @user.valid_password?(params[:user][:password]) && @user.is_active
+  
+    # ログイン成功時の処理
+    # セッションにユーザーIDを保存
+    session[:user_id] = @user.id 
+    # ログイン後のデフォルトページにリダイレクトする
+    redirect_to root_path 
+  end
+
+
+  def prohibit_multiple_login
+    redirect_to admin_root_path
+  end
 end
