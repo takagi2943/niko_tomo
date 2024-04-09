bea148cd1ad4228d23e79ae60456521b0c21fe44
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-04-09 02:18:31 +0000

    共通テンプレート作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -16,31 +16,39 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     get "/about" => "homes#about"
-    # 音楽共有
-    resources :music_posts, only: [:index, :show, :create] do
-      resource :favorites, only: [:index,:create, :destroy]
-      resources :music_post_comments, only:[:create, :destroy, :update]
+    resources :users do
+        # ユーザー検索用
+        collection do
+          get 'search'
+        end
+      # 音楽共有
+      resources :music_posts, only: [:index, :show, :create] do
+        resource :favorites, only: [:index,:create, :destroy]
+        resources :music_post_comments, only:[:create, :destroy, :update]
+      end
+      # ユーザー情報
+      resources :users, only: [:show, :edit, :update, :destroy] do
+        get 'confirm', on: :collection
+        resources :relationships, only: [:create, :destroy]
+          get 'followings' => 'relationships#followings', as: 'followings'
+          get 'followers' => 'relationships#followers', as: 'followers'
+      end
+      # 探究室
+      resources :labos, only: [:index, :show, :create, :update, :destroy] do
+        resources :labo_comments, only: [:destroy]
+      end 
+      # グループ
+      resources :groups, only: [:index, :show, :create, :update] do
+        resources :group_user_comments, only: [:create, :destroy]
+      end 
     end
-    # ユーザー情報
-    resources :users, only: [:show, :edit, :update, :destroy] do
-      get 'confirm', on: :collection
-      resources :relationships, only: [:create, :destroy]
-        get 'followings' => 'relationships#followings', as: 'followings'
-        get 'followers' => 'relationships#followers', as: 'followers'
-    end
-    # 探究室
-    resources :labos, only: [:index, :show, :create, :update, :destroy] do
-      resources :labo_comments, only: [:destroy]
-    end 
-    # グループ
-    resources :groups, only: [:index, :show, :create, :update] do
-      resources :group_user_comments, only: [:create, :destroy]
-    end 
   end
   
   # 管理者側
   namespace :admin do
     root to: "homes#top"
+    # 検索用
+    get "/search" => "homes#search"
     # 会員情報
     resources :users, only: [:index, :show, :edit, :destroy] do
       resources :user_post_comments, only: [:index, :destroy]
