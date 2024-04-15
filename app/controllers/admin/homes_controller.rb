e924d76863325863437fb72f0a813e6d75a70b5f
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-04-15 08:49:28 +0000

    admin周り作成

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,16 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!, except: :root
 
   def top
+    @users = User.all
+    @user_name = User.order('id DESC').limit(4)
+    @groups = Group.all
+    @group_name = Group.order('id DESC').limit(4)
+    @music_posts = MusicPost.all
+    @music_post_name = MusicPost.order('id DESC').limit(4)
+    @labos = Labo.all
+    @labo_name = Labo.order('id DESC').limit(4)
+    @total_active_users = User.where(active: true).size
+    @total_inactive_users = User.where(active: false).size
+    @total_users = User.size
   end
 end
