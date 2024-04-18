cae1e1f6d6a55dd5792c90bd5eb2a45866110a3f
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-04-18 11:39:12 +0000

    fix admin::LaboComments

--- app/controllers/admin/labo_comments_controller.rb
+++ app/controllers/admin/labo_comments_controller.rb
@@ -1,9 +1,12 @@
 class Admin::LaboCommentsController < ApplicationController
-  before_action :authenticate_admin!, except: :root
-  
+  before_action :authenticate_admin!
+
   def index
+    @labo_comments = LaboComment.all
+    @labo_comment = LaboComment.page(params[:page])
   end
 
   def show
+    @labo_comment = LaboComment.find(params[:id])
   end
 end
