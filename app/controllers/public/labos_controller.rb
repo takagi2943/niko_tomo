61c0adbd8b7c63e2411c78356bb6cc116d8d930e
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-04-19 15:47:38 +0000

    会員側の探究室・会員一覧などを作成

--- app/controllers/public/labos_controller.rb
+++ app/controllers/public/labos_controller.rb
@@ -4,10 +4,34 @@ before_action :authenticate_user!, only: [:create, :update, :destroy]
   def index
     @users = User.all
     @user = current_user
+    @labo = Labo.new
     @tags = Tag.all
     @labo_comment = LaboComment.all
+    @labo_comments = LaboComment.page(params[:page]) #ページネーション
   end
 
   def show
+    @labo_comment = LaboComment.find(params[:id])
   end
+  
+  def create
+    @labo_comment = LaboComment.new(labo_comment_params)
+    @tags = Tag.all
+    if @labo_comment.save
+      redirect_to labos_path
+    else 
+      render 'public/labos/index'
+    end
+  end
+  
+  def destroy
+    @labo_comment = LaboComment.find(params[:id])
+    @labo_comment.destroy
+  end 
+  
+  private
+  
+  def labo_comment_params
+    params.require(labo_comment).parmit(:comment, :tag_id)
+  end 
 end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,8 +1,8 @@
 class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
-  
+
   	def search
   	  @genre_id = @params[:genre_id]
   	  @tags = Tag.wher(genre_id: @tag_id)
-  	end 
+  	end
 end
