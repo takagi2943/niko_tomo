59c4b29298c5221f6006c208570f429644f82414
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-04-19 00:58:55 +0000

    admin周り修正

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,3 +1,16 @@
 class Admin::SearchesController < ApplicationController
   before_action :authenticate_admin!
-end
+  
+  def search
+    @model = params[:model]
+    @content = params[:content]
+    @method =params[:method]
+  
+    # 選択したモデルに応じて検索を実行
+    if @model == 'user'
+      @records = User.search_for(@content, @method)
+    else
+      @records = MusicPost.search_for(@content, @method)
+    end
+  end
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -97,4 +97,5 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user.id)
   end
+  
 end
