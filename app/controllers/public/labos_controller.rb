6a955cd96aa0122474824b3d9afe55df019d1890
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-04-20 00:14:04 +0000

    labo作成

--- app/controllers/public/labos_controller.rb
+++ app/controllers/public/labos_controller.rb
@@ -13,25 +13,25 @@ before_action :authenticate_user!, only: [:create, :update, :destroy]
   def show
     @labo_comment = LaboComment.find(params[:id])
   end
-  
+
   def create
     @labo_comment = LaboComment.new(labo_comment_params)
     @tags = Tag.all
     if @labo_comment.save
       redirect_to labos_path
-    else 
+    else
       render 'public/labos/index'
     end
   end
-  
+
   def destroy
     @labo_comment = LaboComment.find(params[:id])
     @labo_comment.destroy
-  end 
-  
+  end
+
   private
-  
+
   def labo_comment_params
-    params.require(labo_comment).parmit(:comment, :tag_id)
-  end 
+    params.require(:labo_comment).permit(:comment, :tag_id)
+  end
 end
