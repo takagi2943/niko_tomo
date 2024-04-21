91d5e1a625a8dcd21241fe07b751495160bb5af8
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-04-21 06:53:31 +0000

    seed追加

--- app/models/labo.rb
+++ app/models/labo.rb
@@ -1,12 +1,12 @@
 class Labo < ApplicationRecord
-  
+
   # タグ/探究室と探究コメントを繋ぐ
   has_many :tag_labos, dependent: :destroy
   has_many :tags, through: :tag_labos
   has_many :labo_comments, dependent: :destroy
   belongs_to :user
-  
+
   validates :body, presence: true, length: { maximum: 200 }
-  
-  
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,47 @@ Admin.create!(
 )
 
 # 会員情報
+User.create!(
+  [
+    { nickname: '二胡爺', email: 'g@g', is_active: true, gender: '男性', introduction: '初めまして', password: '123123', birthdate: '1968/4/20' },
+    { nickname: '二胡姫', email: 'j@j', is_active: true, gender: '女性', introduction: '初めましてよろしくね', password: '123123', birthdate: '1988/2/12' },
+    { nickname: 'もっちー', email: 'q@q', is_active: true, gender: 'その他', introduction: '初めまして', password: '123123', birthdate: '1968/4/21' },
+    { nickname: '二胡にぃ', email: 'k@k', is_active: true, gender: '男性', introduction: '初めまして', password: '123123', birthdate: '1968/4/21' },
+    { nickname: '二胡姉', email: 'm@m', is_active: true, gender: '男性', introduction: '初めまして', password: '123123', birthdate: '1968/4/21' },
+    { nickname: '二胡人', email: 'w@w', is_active: true, gender: 'その他', introduction: '初めまして', password: '123123', birthdate: '1968/4/16' },
+    { nickname: 'かっきー', email: 'y@y', is_active: true, gender: '女性', introduction: '初めまして', password: '123123', birthdate: '1968/4/5' },
+    { nickname: 'つっちー', email: 'r@r', is_active: true, gender: 'その他', introduction: 'こんにちは', password: '123123', birthdate: '1968/4/22' },
+    { nickname: 'ムッチー', email: 'x@x', is_active: true, gender: '男性', introduction: '初めまして', password: '123123', birthdate: '1968/4/22' },
+    { nickname: 'マオマオ', email: 'z@z', is_active: true, gender: 'その他', introduction: '初めまして', password: '123123', birthdate: '1968/4/22' }
+  ]
+)
+
+MusicPost.create!(
+  [
+    { user_id: 1, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 2, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 3, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 4, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 5, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 6, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 7, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 8, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 9, title:'二胡始めました。', body: '記録として始めようと思います。' },
+    { user_id: 10, title:'二胡始めました。', body: '記録として始めようと思います。' }
+    ]
+  )
+
+Niko.create!(
+  [
+    { user_id: 1, is_favorite: true, name: 'にーにー', year: 2, feature: '紫檀'},
+    { user_id: 2, is_favorite: true, name: 'しーた', year: 2, feature: '紫檀'},
+    { user_id: 3, is_favorite: true, name: 'こく', year: 2, feature: '黒檀'},
+    { user_id: 4, is_favorite: true, name: 'ベニマル', year: 2, feature: '老紅木'},
+    { user_id: 5, is_favorite: true, name: '二胡', year: 2, feature: '紫檀'},
+    { user_id: 6, is_favorite: true, name: 'バリュー', year: 2, feature: '紫檀'},
+    { user_id: 7, is_favorite: true, name: 'ローム', year: 2, feature: '紫檀'},
+    { user_id: 8, is_favorite: true, name: 'しーちゃん', year: 2, feature: '紫檀'},
+    { user_id: 9, is_favorite: true, name: 'シオン', year: 2, feature: '老紅木'},
+    { user_id: 10, is_favorite: true, name: 'くろ', year: 2, feature: '紫檀'}
+  ]
+ )
